8d975a458c1c07617e8225ee889c41768346621a
EstefaniSoto <eri022004@hotmail.com>
2024-09-12 17:16:53 -0400

    Ajuste de vite para el import del .env

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Usa la variable de entorno para definir la URL base
-const API_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5054/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5054/api';
 
 // Función para obtener la lista de contribuyentes
 // Hace una solicitud GET a la ruta '/contribuyentes' en la API
